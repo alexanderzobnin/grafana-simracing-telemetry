521785d961b5c4e5cced8fcefb639d69763d9675
Alexander Zobnin <alexanderzobnin@gmail.com>
2022-05-06 14:49:43 +0300

    Fix package name in main.go

--- pkg/main.go
+++ pkg/main.go
@@ -7,6 +7,8 @@ import (
 	"github.com/grafana/grafana-plugin-sdk-go/backend/log"
 )
 
+const PLUGIN_ID = "alexanderzobnin-simracingtelemetry-datasource"
+
 func main() {
 	// Start listening to requests sent from Grafana. This call is blocking so
 	// it won't finish until Grafana shuts down the process or the plugin choose
@@ -16,7 +18,7 @@ func main() {
 	// from Grafana to create different instances of SimracingTelemetryDatasource (per datasource
 	// ID). When datasource configuration changed Dispose method will be called and
 	// new datasource instance created using NewSimracingTelemetryDatasource factory.
-	if err := datasource.Manage("simracing-telemetry-datasource", NewSimracingTelemetryDatasource, datasource.ManageOpts{}); err != nil {
+	if err := datasource.Manage(PLUGIN_ID, NewSimracingTelemetryDatasource, datasource.ManageOpts{}); err != nil {
 		log.DefaultLogger.Error(err.Error())
 		os.Exit(1)
 	}
